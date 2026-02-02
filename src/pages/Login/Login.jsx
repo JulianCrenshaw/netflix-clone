5d51bdd4720eac10f7065d778545aea4dc1a5fd2
Julian <julian.gold.crenshaw@gmail.com>
2026-02-02 07:41:03 -0600

    sign up

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,8 +7,20 @@ const Login = () => {
     <div className='login'>
       <img src={logo} className='login-logo' alt="" />
       <div className="login-form">
-        <h1>Sign In</h1>
-        <form action=""></form>
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder='Your Name'/>
+          <input type="email" placeholder='email'/>
+          <input type="password" placeholder='password'/>
+          <button>Sign Up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help</p>
+          </div>
+        </form>
       </div>
     </div>
   )
