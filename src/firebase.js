3b95d7c30a18e6d1fc87478f71b4c630205d2846
Julian <julian.gold.crenshaw@gmail.com>
2026-02-05 06:57:11 -0600

    user

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDhQF5yNTASIxpK1mdn5i3uW-aLAXTCdrU",
@@ -17,3 +17,20 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+const signup = async (name, email, password) => {
+    try {
+       const res = await createUserWithEmailAndPassword(auth, email, 
+        password);
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid, 
+            name, 
+            authProvider: "local", 
+            email
+        });
+    }
+    catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
